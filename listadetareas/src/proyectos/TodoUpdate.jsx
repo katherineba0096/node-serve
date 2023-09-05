214a13748d977af6b14e5bf8b162b740834b5aae
katherineba0096 <kbalanta4@misena.edu.co>
2023-09-05 16:31:12 -0500

    lista de tareas

---
+++ listadetareas/src/proyectos/TodoUpdate.jsx
@@ -0,0 +1,47 @@
+import {FaEdit} from "react-icons/fa"
+import { useForm } from "../hooks/useform";
+import { useRef, useState } from "react";
+
+
+export const TodoUpdate = ({ todo, handleUpdateTodo }) => {
+	const { updateDescription, onInputChange } = useForm({
+		updateDescription: todo.description,
+	});
+
+	const [disabled, setDisabled] = useState(true);
+	const focusInputRef = useRef();
+
+	const onSubmitUpdate = e => {
+		e.preventDefault();
+
+		const id = todo.id;
+		const description = updateDescription;
+
+		handleUpdateTodo(id, description);
+
+		setDisabled(!disabled);
+
+		focusInputRef.current.focus();
+	};
+
+	return (
+		<form onSubmit={onSubmitUpdate}>
+			<input
+				type='text'
+				className={`input-update ${
+					todo.done ? 'text-decoration-dashed' : ''
+				}`}
+				name='updateDescription'
+				value={updateDescription}
+				onChange={onInputChange}
+				placeholder='¿Qué hay que hacer?'
+				readOnly={disabled}
+				ref={focusInputRef}
+			/>
+
+			<button className='btn-edit' type='submit'>
+				<FaEdit />
+			</button>
+		</form>
+	);
+};
