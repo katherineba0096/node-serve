e1bea58dafc5e0fceede88abf745bfde09989f6f
katherineba0096 <kbalanta4@misena.edu.co>
2023-10-25 21:34:47 -0500

    lista de tareas actualizacion

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './App.css'
+import { useTodo } from './hooks/useTodo'
+import { Poject } from './componentes/Poject'
+import { TodoAdd } from './componentes/TodoAdd'
+
+function App() {
+	const {
+		todos,
+		todosCount,
+		pendingTodosCount,
+		handleNewTodo,
+		handleDeleteTodo,
+		handleCompleteTodo,
+		handleUpdateTodo,
+	} = useTodo();
+
+	return (
+		<>
+			<div className='card-to-do'>
+				<h1>Lista de tareas</h1>
+				<div className='counter-todos'>
+					<h3>
+						N° Tareas: <span>{todosCount}</span>
+					</h3>
+					<h3>
+						Pendientes: <span>{pendingTodosCount}</span>
+					</h3>
+				</div>
+
+				<div className='add-todo'>
+					<h3>Añadir Tarea</h3>
+					<TodoAdd handleNewTodo={handleNewTodo} />
+				</div>
+
+				<Poject
+					todos={todos}
+					handleUpdateTodo={handleUpdateTodo}
+					handleDeleteTodo={handleDeleteTodo}
+					handleCompleteTodo={handleCompleteTodo}
+				/>
+			</div>
+		</>
+	);
+}
+
+
+export default App
